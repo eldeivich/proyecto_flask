c2d10b3a777a0088210319a215e189140796e663
eldeivich <47783040+eldeivich@users.noreply.github.com>
2020-01-28 17:47:05 +0100

    Primer esbozo de app no funcional

--- cryptoapp/routes.py
+++ cryptoapp/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/purchase")
+def purchase():
+    return render_template("purchase.html")
+
+@app.route("/status")
+def status():
+    return render_template("status.html")
