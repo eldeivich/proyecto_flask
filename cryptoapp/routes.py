e46e15bedcd7a322ae6961eeac5ede12dfac86b4
eldeivich <47783040+eldeivich@users.noreply.github.com>
2020-01-31 10:59:46 +0100

    implementacion de api devuelve json con monedas

--- cryptoapp/routes.py
+++ cryptoapp/routes.py
@@ -1,12 +1,57 @@
 from cryptoapp import app
 from flask import render_template
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout
+import json
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+
+    parameters = {
+        'symbol' : 'BTC,ETH,XRP,LTC,BCH,BNB,USDT,EOS,BSV,XLM,ADA,TRX'
+    }
+
+    headers = {
+        'Accepts' : 'application/json',
+        'X-CMC_PRO_API_KEY': 'a149b700-87b4-4f94-8c08-ea7d6bcdc9df'
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        print(data)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
     return render_template("index.html")
 
-@app.route("/purchase")
+@app.route("/purchase", methods=['GET', 'POST'])
 def purchase():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+
+    parameters = {
+        'symbol' : 'BTC,ETH,XRP,LTC,BCH,BNB,USDT,EOS,BSV,XLM,ADA,TRX'
+    }
+
+    headers = {
+        'Accepts' : 'application/json',
+        'X-CMC_PRO_API_KEY': 'a149b700-87b4-4f94-8c08-ea7d6bcdc9df'
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        print(data)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
     return render_template("purchase.html")
 
 @app.route("/status")
