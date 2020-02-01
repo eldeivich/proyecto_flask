09df506ed5421d2a62d13e88f3509ed81e311100
eldeivich <47783040+eldeivich@users.noreply.github.com>
2020-02-01 18:29:19 +0100

    algunos avances aunque pocos, añadido base.html con jinja y dado algunos formatos

--- cryptoapp/routes.py
+++ cryptoapp/routes.py
@@ -1,11 +1,14 @@
 from cryptoapp import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import requests
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout
 import json
+import sqlite3
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
+BASE_DATOS = './data/data.db'
+
+def tablaCryptos():
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
 
     parameters = {
@@ -27,15 +30,33 @@ def index():
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         print(e)
 
+    
+
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    query = "INSERT into cryptos (id, symbol, name) values(?, ?, ?);"
+    for insert in data['data']:
+        cursor.execute(query, (insert['id'], insert['symbol'], insert['name']))
+    conn.commit()
+    conn.close()
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
     return render_template("index.html")
 
 @app.route("/purchase", methods=['GET', 'POST'])
 def purchase():
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+    if request.method == 'GET':
+        print('estoy en GET')
+        return render_template("purchase.html")
+    
+    desde = request.values.get('from')
+    convertir_a = request.values.get('to')
+    cuantia = int(request.values.get('q'))
 
-    parameters = {
-        'symbol' : 'BTC,ETH,XRP,LTC,BCH,BNB,USDT,EOS,BSV,XLM,ADA,TRX'
-    }
+    url_consulta = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'
+    parametros = {'amount': cuantia, 'symbol': desde, 'convert': convertir_a}
+    parametros_unidad = {'amount': 1, 'symbol': desde, 'convert': convertir_a}
 
     headers = {
         'Accepts' : 'application/json',
@@ -44,15 +65,29 @@ def purchase():
 
     session = Session()
     session.headers.update(headers)
-
+    
     try:
-        response = session.get(url, params=parameters)
-        data = json.loads(response.text)
-        print(data)
-    except (ConnectionError, Timeout, TooManyRedirects) as e:
-        print(e)
+        respuesta = session.get(url_consulta, params=parametros)
+        respuesta_unidad = session.get(url_consulta,params=parametros_unidad)
+        consulta = json.loads(respuesta.text)
+        consulta_unidad = json.loads(respuesta_unidad.text)
+        print('----------------------------------------------------')
+        print(consulta)
+        print('----------------------------------------------------')
+        print(consulta_unidad)
+    except(ConnectionError, timeout, TooManyRedirects) as e2:
+        print(e2)
 
-    return render_template("purchase.html")
+    print('method:', request.method)
+    print('parameters:', request.values)
+
+    qcuantity = consulta['data']['quote'][convertir_a]['price']
+    qcuantity_unitario = consulta_unidad['data']['quote'][convertir_a]['price']
+    print('la cantidad es: ', qcuantity, 'Y el precio unitario: ', qcuantity_unitario)
+
+    return render_template("purchase.html", qcuantity = qcuantity, qcuantity_unitario = qcuantity_unitario)
+    
+    
 
 @app.route("/status")
 def status():
