c3f16cf90edc2a28bd6ca87311a0e83f0853a515
eldeivich <47783040+eldeivich@users.noreply.github.com>
2020-02-05 20:20:01 +0100

    proyecto hasta ahora

--- cryptoapp/routes.py
+++ cryptoapp/routes.py
@@ -51,51 +51,53 @@ def purchase():
     form = CryptoForm(request.form)
 
     if request.method == 'GET':
-        print('estoy en GET')
         return render_template("purchase.html", form = form)
 
-    if form.validate():
-        desde = request.form.get('desde')
-        convertir_a = request.form.get('convertir_a')
-        cuantia = int(request.form.get('cuantia'))
+    if form.calc.data:
+        if form.validate():
+            desde = request.form.get('desde')
+            convertir_a = request.form.get('convertir_a')
+            cuantia = int(request.form.get('cuantia'))
 
-        url_consulta = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'
-        parametros = {'amount': cuantia, 'symbol': desde, 'convert': convertir_a}
-        parametros_unidad = {'amount': 1, 'symbol': desde, 'convert': convertir_a}
+            url_consulta = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'
+            parametros = {'amount': cuantia, 'symbol': desde, 'convert': convertir_a}
+            parametros_unidad = {'amount': 1, 'symbol': desde, 'convert': convertir_a}
 
-        headers = {
-            'Accepts' : 'application/json',
-            'X-CMC_PRO_API_KEY': 'a149b700-87b4-4f94-8c08-ea7d6bcdc9df'
-        }
+            headers = {
+                'Accepts' : 'application/json',
+                'X-CMC_PRO_API_KEY': 'a149b700-87b4-4f94-8c08-ea7d6bcdc9df'
+            }
 
-        session = Session()
-        session.headers.update(headers)
-        
-        try:
-            respuesta = session.get(url_consulta, params=parametros)
-            respuesta_unidad = session.get(url_consulta,params=parametros_unidad)
-            consulta = json.loads(respuesta.text)
-            consulta_unidad = json.loads(respuesta_unidad.text)
-            print('----------------------------------------------------')
-            print(consulta)
-            print('----------------------------------------------------')
-            print(consulta_unidad)
-        except(ConnectionError, timeout, TooManyRedirects) as e2:
-            print(e2)
+            session = Session()
+            session.headers.update(headers)
+            
+            try:
+                respuesta = session.get(url_consulta, params=parametros)
+                respuesta_unidad = session.get(url_consulta,params=parametros_unidad)
+                consulta = json.loads(respuesta.text)
+                consulta_unidad = json.loads(respuesta_unidad.text)
+                print('----------------------------------------------------')
+                print(consulta)
+                print('----------------------------------------------------')
+                print(consulta_unidad)
+            except(ConnectionError, timeout, TooManyRedirects) as e2:
+                print(e2)
 
-        print('method:', request.method)
-        print('parameters:', request.values)
+            print('method:', request.method)
+            print('parameters:', request.values)
 
-        qcuantity = consulta['data']['quote'][convertir_a]['price']
-        qcuantity_unitario = consulta_unidad['data']['quote'][convertir_a]['price']
-        hora = time.strftime('%H:%M:%S')
-        fecha = time.strftime('%d/%m/%Y')
-        print('la cantidad es: ', qcuantity, 'Y el precio unitario: ', qcuantity_unitario)
-        print('fecha: ', fecha, ' y hora: ', hora)
+            qcuantity = consulta['data']['quote'][convertir_a]['price']
+            qcuantity_unitario = consulta_unidad['data']['quote'][convertir_a]['price']
+            hora = time.strftime('%H:%M:%S')
+            fecha = time.strftime('%d/%m/%Y')
+            print('la cantidad es: ', qcuantity, 'Y el precio unitario: ', qcuantity_unitario)
+            print('fecha: ', fecha, ' y hora: ', hora)
 
-        return render_template("purchase.html", form=form, qcuantity = qcuantity, qcuantity_unitario = qcuantity_unitario)
-    else:
-        return render_template("purchase.html", form=form)
+            return render_template("purchase.html", form=form, qcuantity = qcuantity, qcuantity_unitario = qcuantity_unitario)
+        else:
+            return render_template("purchase.html", form=form)
+    elif form.ok.data:
+        pass
 
 @app.route("/status")
 def status():
