572d848eb31c214b5c17f5f1476de5c9e04d32b8
eldeivich <47783040+eldeivich@users.noreply.github.com>
2020-02-10 15:51:42 +0100

    Implementada función cartera y gestión de errores  con monedas disponibles

--- cryptoapp/forms.py
+++ cryptoapp/forms.py
@@ -2,15 +2,59 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SubmitField, HiddenField, SelectField, IntegerField
 from wtforms.validators import DataRequired, ValidationError
 from wtforms.widgets import Select
+import sqlite3
+
+BASE_DATOS = './data/data.db'
+CHOICES = (('EUR', 'Euros'), ('BTC', 'Bitcoin'), ('LTC', 'Litecoin'), ('XRP', 'XRP'), ('XLM', 'Stellar'), ('USDT', 'Tether'), ('ETH', 'Ethereum'), ('EOS', 'EOS'), ('BCH', 'Bitcoin Cash'), ('BNB', 'Binance Coin'), ('TRX', 'Tron'), ('ADA', 'Cardano'), ('BSV', 'Bitcoin SV'))
+CRYPTOS = dict(CHOICES)
+MONEDAS = {'EUR': 0, 'BTC': 0, 'LTC': 0, 'XRP': 0, 'XLM': 0, 'USDT': 0, 'ETH': 0, 'EOS': 0, 'BCH': 0, 'BNB': 0, 'TRX': 0, 'ADA': 0, 'BSV': 0}
+
+def cartera():
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    query = "SELECT * from movements;"
+    filas = cursor.execute(query)
+
+    for fila in filas:
+        if fila[3] == 'EUR' and fila[5] == 'BTC':
+            MONEDAS['BTC'] += fila[6]
+        if fila[3] == 'BTC' and fila[5] != 'EUR':
+            MONEDAS['BTC'] -= fila[4]
+            MONEDAS[fila[5]] += fila[6]
+
+
+    conn.close()
+    return MONEDAS
 
 def error_cuantia(form, field):
     if form.cuantia.errors:
         raise ValidationError('Este campo debe ir rellenado con números.')
 
+def error_iguales(form, field):
+    if form.convertir_a.data == form.desde.data:
+        raise ValidationError('Los campos From y To no deben ser iguales')
+
+def error_euro_no_btc(form, field):
+    if form.desde.data == 'EUR' and form.convertir_a.data != 'BTC':
+        raise ValidationError('Desde Euros solo se puede convertir a BitCoin')
+
+def error_crypto_no_euro(form, field):
+    if form.desde.data != 'BTC' and form.convertir_a.data == 'EUR':
+        raise ValidationError('No se puede convertir a euros una crypto que no sea BitCoin')
+
+def cantidad_disponible(form, field):
+    disponible = cartera()
+    if form.desde.data == 'BTC' and form.convertir_a.data != 'EUR':
+        if disponible[form.desde.data] < form.cuantia.data:
+            raise ValidationError('No Tienes BitCoins suficientes, Realiza una transacción de Euros a BitCoin')
+    if form.desde.data != 'EUR':
+        if disponible[form.desde.data] < form.cuantia.data:
+            raise ValidationError('No tienes {} suficientes, Realiza primero una transacción de otra moneda en la que tengas credito suficiente a {}.'.format(CRYPTOS[form.desde.data], CRYPTOS[form.desde.data]))
+
 class CryptoForm(FlaskForm):
-    desde = SelectField('From', validators=[DataRequired()], choices=[('EUR', 'Euros'), ('BTC', 'Bitcoin'), ('LTC', 'Litecoin'), ('XRP', 'XRP'), ('XLM', 'Stellar'), ('USDT', 'Tether'), ('ETH', 'Ethereum'), ('EOS', 'EOS'), ('BCH', 'Bitcoin Cash'), ('BNB', 'Binance Coin'), ('TRX', 'Tron'), ('ADA', 'Cardano'), ('BSV', 'Bitcoin SV')])
-    convertir_a = SelectField('To', validators=[DataRequired()], choices=[('EUR', 'Euros'), ('BTC', 'Bitcoin'), ('LTC', 'Litecoin'), ('XRP', 'XRP'), ('XLM', 'Stellar'), ('USDT', 'Tether'), ('ETH', 'Ethereum'), ('EOS', 'EOS'), ('BCH', 'Bitcoin Cash'), ('BNB', 'Binance Coin'), ('TRX', 'Tron'), ('ADA', 'Cardano'), ('BSV', 'Bitcoin SV')])
-    cuantia = IntegerField('Q', validators=[DataRequired(), error_cuantia])
+    desde = SelectField('From', validators=[DataRequired(), error_iguales, error_euro_no_btc], choices=CHOICES)
+    convertir_a = SelectField('To', validators=[DataRequired(), error_iguales, error_crypto_no_euro], choices=CHOICES)
+    cuantia = IntegerField('Q', validators=[DataRequired(), error_cuantia, cantidad_disponible])
 
     calc = SubmitField('Calcular')
 
--- cryptoapp/routes.py
+++ cryptoapp/routes.py
@@ -9,6 +9,7 @@ import sqlite3
 import time
 
 BASE_DATOS = './data/data.db'
+API_KEY= app.config['API_KEY']
 
 def tablaCryptos():
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
@@ -19,7 +20,7 @@ def tablaCryptos():
 
     headers = {
         'Accepts' : 'application/json',
-        'X-CMC_PRO_API_KEY': 'a149b700-87b4-4f94-8c08-ea7d6bcdc9df'
+        'X-CMC_PRO_API_KEY': API_KEY
     }
 
     session = Session()
@@ -42,9 +43,55 @@ def tablaCryptos():
     conn.commit()
     conn.close()
 
+def consultaApi(desde, convertir_a, cuantia):
+    url_consulta = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'
+    parametros = {'amount': cuantia, 'symbol': desde, 'convert': convertir_a}
+    parametros_unidad = {'amount': 1, 'symbol': desde, 'convert': convertir_a}
+
+    headers = {
+        'Accepts' : 'application/json',
+        'X-CMC_PRO_API_KEY': API_KEY
+    }
+
+    session = Session()
+    session.headers.update(headers)
+    
+    try:
+        respuesta = session.get(url_consulta, params=parametros)
+        respuesta_unidad = session.get(url_consulta,params=parametros_unidad)
+        consulta = json.loads(respuesta.text)
+        consulta_unidad = json.loads(respuesta_unidad.text)
+        print('----------------------------------------------------')
+        print(consulta)
+        print('----------------------------------------------------')
+        print(consulta_unidad)
+    except(ConnectionError, timeout, TooManyRedirects) as e2:
+        print(e2)
+
+    return consulta, consulta_unidad
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    query = "SELECT * from movements;"
+    filas = cursor.execute(query)
+    
+    listado = []
+    valorUnit = []
+    for fila in filas:
+        fila = list(fila)
+        unidad = consultaApi(fila[3], fila[5], 1)
+        valorUnit = unidad[0]['data']['quote'][fila[5]]['price']
+        fila.append(valorUnit)
+        fila = tuple(fila)
+        listado.append(fila)
+        print(fila)
+    
+    conn.close()
+
+    return render_template("index.html", listado = listado)
 
 @app.route("/purchase", methods=['GET', 'POST'])
 def purchase():
@@ -65,7 +112,7 @@ def purchase():
 
             headers = {
                 'Accepts' : 'application/json',
-                'X-CMC_PRO_API_KEY': 'a149b700-87b4-4f94-8c08-ea7d6bcdc9df'
+                'X-CMC_PRO_API_KEY': API_KEY
             }
 
             session = Session()
@@ -86,8 +133,8 @@ def purchase():
             print('method:', request.method)
             print('parameters:', request.values)
 
-            qcuantity = consulta['data']['quote'][convertir_a]['price']
-            qcuantity_unitario = consulta_unidad['data']['quote'][convertir_a]['price']
+            qcuantity = round(consulta['data']['quote'][convertir_a]['price'], 5)
+            qcuantity_unitario = round(consulta_unidad['data']['quote'][convertir_a]['price'], 5)
             hora = time.strftime('%H:%M:%S')
             fecha = time.strftime('%d/%m/%Y')
             print('la cantidad es: ', qcuantity, 'Y el precio unitario: ', qcuantity_unitario)
@@ -97,8 +144,50 @@ def purchase():
         else:
             return render_template("purchase.html", form=form)
     elif form.ok.data:
-        pass
+        desde = request.form.get('desde')
+        convertir_a = request.form.get('convertir_a')
+        cuantia = int(request.form.get('cuantia'))
+        qcuantity = float(request.form.get('qcuantityh'))
+        qcuantity_unitario = float(request.form.get('qcuantity_unitarioh'))
+        hora = time.strftime('%H:%M:%S')
+        fecha = time.strftime('%d/%m/%Y')
+
+        print('Grabo en base de datos; desde: ', desde, 'convertir a: ', convertir_a, 'cuantía: ', cuantia, 'QCuantity: ', qcuantity, 'Precio Unitario: ', qcuantity_unitario)
+
+        conn = sqlite3.connect(BASE_DATOS)
+        cursor = conn.cursor()
+        query = "INSERT into movements (date, time, from_currency, from_quantity, to_currency, to_quantity) values(?, ?, ?, ?, ?, ?);"
+        cursor.execute(query, (fecha, hora, desde, cuantia, convertir_a, qcuantity))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for("index"))
+
+
+
 
 @app.route("/status")
 def status():
-    return render_template("status.html")
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    query = "SELECT * from movements;"
+    filas = cursor.execute(query)
+
+    e_invertido = ''
+    acumular = ''
+    valor_total = 0
+    for fila in filas:
+        acumular = consultaApi(fila[5], 'EUR', fila[6])
+        valor_total += acumular[0]['data']['quote']['EUR']['price']
+
+        print('----------------------------------------------------------------------')
+        print(acumular)
+        
+        if fila[3] == 'EUR':
+            e_invertido = fila[4]
+        
+    print('Euros invertidos: ', e_invertido, '; Valor Total', valor_total)
+
+    conn.close()
+
+    return render_template("status.html", e_invertido = e_invertido, valor_total = valor_total)
